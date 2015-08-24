3426f995613e0ee8ef87d7775b2cb7737b9a6fd0
Jason Ozias <jason.g.ozias@gmail.com>
2015-08-24 14:49:20 -0400

    docs

---
+++ README.md
@@ -0,0 +1,13 @@
+# node-jdbc-example
+Node JDBC Example Project
+
+## Status
+[![Build Status](https://travis-ci.org/CraZySacX/node-jdbc-example.svg?branch=master)](https://travis-ci.org/CraZySacX/node-jdbc-example)
+
+# Dependencies
+npm and node must be installed to run this project.
+
+# Running
+1. Clone: ```git clone https://github.com/CraZySacX/node-jdbc-example.git```
+2. Dependency Update: ```npm install```
+3. Run: ```npm test || npm run posttest```
--- test/test-jdbc-example.js
+++ test/test-jdbc-example.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+var asyncjs = require('async');
 var nodeunit = require('nodeunit');
 var ex = require('../lib/jdbc-example');
 
@@ -10,9 +12,89 @@ module.exports = {
     });
   },
   testcreate: function(test) {
-    ex.create(null, function(err, result) {
-      test.expect(1);
-      test.equal(result, 0);
+    ex.create("CREATE TABLE blah "
+            + "(id int, name varchar(10), created_date DATE, "
+            +  "created_time TIME, "
+            +  "created_timestamp TIMESTAMP)",
+            function(err, result) {
+              test.expect(1);
+              test.equal(result, 0);
+              test.done();
+            });
+  },
+  testparallelcreates: function(test) {
+    asyncjs.parallel([
+      function(callback) {
+        ex.create("CREATE TABLE blah0 "
+                + "(id int, name varchar(10), created_date DATE, "
+                +  "created_time TIME, "
+                +  "created_timestamp TIMESTAMP)",
+                function(err, result) {
+                  if (err) {
+                    callback(err);
+                  } else {
+                    callback(null, result);
+                  }
+                });
+      },
+      function(callback) {
+        ex.create("CREATE TABLE blah1 "
+                + "(id int, name varchar(10), created_date DATE, "
+                +  "created_time TIME, "
+                +  "created_timestamp TIMESTAMP)",
+                function(err, result) {
+                  if (err) {
+                    callback(err);
+                  } else {
+                    callback(null, result);
+                  }
+                });
+      },
+      function(callback) {
+        ex.create("CREATE TABLE blah2 "
+                + "(id int, name varchar(10), created_date DATE, "
+                +  "created_time TIME, "
+                +  "created_timestamp TIMESTAMP)",
+                function(err, result) {
+                  if (err) {
+                    callback(err);
+                  } else {
+                    callback(null, result);
+                  }
+                });
+      },
+      function(callback) {
+        ex.create("CREATE TABLE blah3 "
+                + "(id int, name varchar(10), created_date DATE, "
+                +  "created_time TIME, "
+                +  "created_timestamp TIMESTAMP)",
+                function(err, result) {
+                  if (err) {
+                    callback(err);
+                  } else {
+                    callback(null, result);
+                  }
+                });
+      },
+      function(callback) {
+        ex.create("CREATE TABLE blah4 "
+                + "(id int, name varchar(10), created_date DATE, "
+                +  "created_time TIME, "
+                +  "created_timestamp TIMESTAMP)",
+                function(err, result) {
+                  if (err) {
+                    callback(err);
+                  } else {
+                    callback(null, result);
+                  }
+                });
+      },
+    ], function(err, results) {
+      test.expect(6);
+      test.equal(results.length, 5);
+      _.each(results, function(result) {
+        test.equal(result, 0);
+      });
       test.done();
     });
   },
