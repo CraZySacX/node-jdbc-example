f11965476e9760c3ff614bcb274ea9782bff3ece
Jason Ozias <jason.g.ozias@gmail.com>
2015-08-26 08:01:05 -0400

    reserve/release wrappers

--- test/test-create.js
+++ test/test-create.js
@@ -4,55 +4,63 @@ var nodeunit = require('nodeunit');
 var ex = require('../lib/jdbc-example');
 
 var pcreate = function(n, callback) {
-  ex.update("CREATE TABLE blah" + n + " "
-          + "(id int, name varchar(10), created_date DATE, "
-          +  "created_time TIME, "
-          +  "created_timestamp TIMESTAMP)",
-          function(err, result) {
-            if (err) {
-              callback(err);
-            } else {
-              callback(null, result);
-            }
-          });
+  ex.derby(function(err, derby) {
+    if (err) {
+      callback(err);
+    } else {
+      ex.update(derby,
+                "CREATE TABLE blah" + n + " "
+              + "(id int, name varchar(10), created_date DATE, "
+              +  "created_time TIME, "
+              +  "created_timestamp TIMESTAMP)",
+              function(err, result) {
+                if (err) {
+                  callback(err);
+                } else {
+                  callback(null, result);
+                }
+              });
+    }
+  });
 };
 
 exports.create = {
-  initderby: function(test) {
-    ex.initDerby(function(err, result) {
-      test.expect(1);
-      test.ok(result);
-      test.done();
+  created: function(test) {
+    ex.derby(function(err, derby) {
+      if (err) {
+        console.log(err);
+      } else {
+        ex.update(derby,
+                  "CREATE TABLE BLAH "
+                + "(id int, name varchar(10), created_date DATE, "
+                +  "created_time TIME, "
+                +  "created_timestamp TIMESTAMP)",
+                function(err, result) {
+                  test.expect(1);
+                  test.equal(result, 0);
+                  test.done();
+                });
+      }
     });
-  },
-  inithsqldb: function(test) {
-    ex.initHSQLDB(function(err, result) {
-      test.expect(1);
-      test.ok(result);
-      test.done();
-    });
-  },
-  create: function(test) {
-    ex.update("CREATE TABLE BLAH "
-            + "(id int, name varchar(10), created_date DATE, "
-            +  "created_time TIME, "
-            +  "created_timestamp TIMESTAMP)",
-            function(err, result) {
-              test.expect(1);
-              test.equal(result, 0);
-              test.done();
-            });
+
   },
   createh: function(test) {
-    ex.updateh("CREATE TABLE BLAH "
-             + "(id int, name varchar(10), created_date DATE, "
-             +  "created_time TIME, "
-             +  "created_timestamp TIMESTAMP)",
-            function(err, result) {
-              test.expect(1);
-              test.equal(result, 0);
-              test.done();
-            });
+    ex.hsqldb(function(err, hsqldb) {
+      if (err) {
+        console.log(err);
+      } else {
+        ex.update(hsqldb,
+                  "CREATE TABLE BLAH "
+                + "(id int, name varchar(10), created_date DATE, "
+                +  "created_time TIME, "
+                +  "created_timestamp TIMESTAMP)",
+                function(err, result) {
+                  test.expect(1);
+                  test.equal(result, 0);
+                  test.done();
+                });
+      }
+    });
   },
   parallelcreates: function(test) {
     asyncjs.parallel([
@@ -71,26 +79,39 @@ exports.create = {
     });
   },
   storedprocedure: function(test) {
-    ex.updateh("CREATE PROCEDURE NEW_BLAH(id int, name varchar(10)) "
-             + "MODIFIES SQL DATA "
-             + "BEGIN ATOMIC "
-             + "  INSERT INTO BLAH VALUES (id, name, CURRENT_DATE, CURRENT_TIME, CURRENT_TIMESTAMP); "
-             + "END;",
-            function(err, result) {
+    ex.hsqldb(function(err, hsqldb) {
+      if (err) {
+        console.log(err);
+      } else {
+        ex.update(hsqldb,
+                  "CREATE PROCEDURE NEW_BLAH(id int, name varchar(10)) "
+                + "MODIFIES SQL DATA "
+                + "BEGIN ATOMIC "
+                + "  INSERT INTO BLAH VALUES (id, name, CURRENT_DATE, CURRENT_TIME, CURRENT_TIMESTAMP); "
+                + "END;",
+                function(err, result) {
+                  test.expect(1);
+                  test.equal(null, err);
+                  test.done();
+                });
+      }
+    });
+  },
+  callproc: function(test) {
+    ex.hsqldb(function(err, hsqldb) {
+      if (err) {
+        console.log(err);
+      } else {
+        ex.prepareCall(hsqldb, "{ call NEW_BLAH(2, 'Another')}", function(err, callablestatement) {
+          if (err) {
+            console.log(err);
+          } else {
+            callablestatement.executeUpdate(function(err, result) {
               test.expect(1);
               test.equal(null, err);
               test.done();
             });
-  },
-  callproc: function(test) {
-    ex.prepareCall("{ call NEW_BLAH(2, 'Another')}", function(err, callablestatement) {
-      if (err) {
-        console.log(err);
-      } else {
-        callablestatement.executeUpdate(function(err, result) {
-          test.expect(1);
-          test.equal(null, err);
-          test.done();
+          }
         });
       }
     });
--- test/test-insert.js
+++ test/test-insert.js
@@ -28,53 +28,60 @@ var insertUser = function(ps, id, callback) {
 var notone = function(n, c) { if (n != 1) { c(true); } else { c(false); }};
 
 exports.insert = {
-  initderby: function(test) {
-    ex.initDerby(function(err, result) {
-      test.expect(1);
-      test.ok(result);
-      test.done();
-    });
-  },
   insert: function(test) {
-    ex.tableexists(null, null, 'BLAH', function(err, exists) {
-      test.expect(3);
-      if (exists) {
-        ex.update("INSERT INTO BLAH "
-                + "VALUES "
-                + "(1, 'Jason', CURRENT_DATE, CURRENT_TIME, CURRENT_TIMESTAMP)",
-                function(err, result) {
-                  test.equal(err, null);
-                  test.ok(result);
-                  test.equal(result, 1);
-                  test.done();
-                });
+    ex.derby(function(err, derby) {
+      if (err) {
+        console.log(err);
       } else {
-        console.log("TABLE 'BLAH' DOES NOT EXIST");
-        test.done();
+        ex.tableexists(derby, null, null, 'BLAH', function(err, exists) {
+          test.expect(3);
+          if (exists) {
+            ex.update(derby,
+                      "INSERT INTO BLAH "
+                    + "VALUES "
+                    + "(1, 'Jason', CURRENT_DATE, CURRENT_TIME, CURRENT_TIMESTAMP)",
+                    function(err, result) {
+                      test.equal(err, null);
+                      test.ok(result);
+                      test.equal(result, 1);
+                      test.done();
+                    });
+          } else {
+            console.log("TABLE 'BLAH' DOES NOT EXIST");
+            test.done();
+          }
+        });
       }
     });
   },
-  onethousandinserts: function(test) {
-    ex.prepare("INSERT INTO BLAH "
-             + "VALUES "
-             + "(?, ?, CURRENT_DATE, CURRENT_TIME, CURRENT_TIMESTAMP)",
-      function(err, preparedstatement) {
-        asyncjs.times(1000, function(n, next) {
-          insertUser(preparedstatement, n, function(err, result) {
-            next(err, result);
-          });
-        }, function(err, results) {
-          if (err) {
-            console.log(err);
-          } else {
-            test.expect(2);
-            test.equal(results.length, 1000);
-            asyncjs.filter(results, notone, function(results) {
-              test.equal(results.length, 0);
-              test.done();
+  _1000inserts: function(test) {
+    ex.derby(function(err, derby) {
+      if (err) {
+        console.log(err);
+      } else {
+        ex.prepare(derby,
+                   "INSERT INTO BLAH "
+                 + "VALUES "
+                 + "(?, ?, CURRENT_DATE, CURRENT_TIME, CURRENT_TIMESTAMP)",
+          function(err, preparedstatement) {
+            asyncjs.times(1000, function(n, next) {
+              insertUser(preparedstatement, n, function(err, result) {
+                next(err, result);
+              });
+            }, function(err, results) {
+              if (err) {
+                console.log(err);
+              } else {
+                test.expect(2);
+                test.equal(results.length, 1000);
+                asyncjs.filter(results, notone, function(results) {
+                  test.equal(results.length, 0);
+                  test.done();
+                });
+              }
             });
-          }
-        });
+          });
+      }
     });
   },
 };
